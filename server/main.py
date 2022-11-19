0aafbd9e2895d1dd829447b0478afbd5b003f15f
Leam Murphy <leam.murpy1@gmail.com>
2022-11-19 15:38:51 -0700

    main openai functionality implemented.

--- server/main.py
+++ server/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
-
+from  helpers import *
 app = FastAPI()
-
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    payload = "Today I enjoyed using blocks in math class because it made a lot of sense. Math on the white board is boring because I can't see"
+    result = query(payload)
+    required_response = result["choices"]
+    return required_response
